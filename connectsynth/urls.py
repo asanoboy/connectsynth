e066860c5b6780bf5f907dfe91a55f34db3f4355
root <root@localhost.localdomain>
2012-10-14 03:38:40 -0700

    a

--- connectsynth/urls.py
+++ connectsynth/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'connectsynth.views.home', name='home'),
-    url(r'^hoge/$', 'hoge.views.hoge_home', name='hoge_home'),
     # url(r'^connectsynth/', include('connectsynth.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -15,4 +13,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^sdk/', include('sdk.urls')),
+    #url(r'^plugin/', include('plugin.urls')),
+    url(r'^auth/', include('autho.urls')),
+    url(r'^account/', include('account.urls')),
+    url(r'', include('public.urls')),
 )
