723d9ee740596f81c7ecdc6b6e1e364eaf23e68a
Yosuke Asano <y.a.personal@gmail.com>
2012-12-03 00:56:49 +0900

    change

--- apps/autho/views.py
+++ apps/autho/views.py
@@ -121,11 +121,12 @@ def autho_twitter_auth_handler(request):
     if url:
         request.session['next'] = url
     
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET, "%s://%s%s" % (
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    """, "%s://%s%s" % (
                             "https", 
                             request.META['HTTP_HOST'], 
                             reverse("autho_twitter_callback")) )
-    
+    """
     try:
         auth_url = auth.get_authorization_url()
     except tweepy.TweepError:
