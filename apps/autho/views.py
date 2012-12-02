48dcbe15b457ced82d28bd16b1266c5b00449efc
Yosuke Asano <y.a.personal@gmail.com>
2012-12-03 01:24:41 +0900

    change

--- apps/autho/views.py
+++ apps/autho/views.py
@@ -167,8 +167,7 @@ def autho_twitter_callback_handler(request):
     #return HttpResponse( reduce(lambda a, b: a+"<br/>"+b, dir(me)) )
     
     if twitter_login(request, me):#me.id, me.screen_name):
-        #return HttpResponseRedirect( request.session.get('next') or reverse("toppage") )
-        return HttpResponseRedirect( reverse("toppage") )
+        return HttpResponseRedirect( request.session.get('next') or reverse("toppage") )
     else:
         return HttpResponse( "invalid access" )
 
