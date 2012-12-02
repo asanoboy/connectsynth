d1a129d6d114de7be37e37b6de763bf6b45e4938
Yosuke Asano <y.a.personal@gmail.com>
2012-12-02 17:03:46 +0900

    change

--- connectsynth/settings.py
+++ connectsynth/settings.py
@@ -7,6 +7,6 @@ if os.environ.has_key('AWS_ACCESS_KEY_ID') and os.environ.has_key('AWS_SECRET_AC
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-if os.environ.has_key("DEBUG") and os.environ.has_key("DEBUG")=="1":
+if os.environ.has_key("DEBUG") and os.environ["DEBUG"]=="1":
     DEBUG = True
     
