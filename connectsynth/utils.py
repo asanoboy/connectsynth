915d37b5974b1114b6ded0ea884ac73349a9945c
Yosuke Asano <y.a.personal@gmail.com>
2012-12-03 22:15:18 +0900

    change

--- connectsynth/utils.py
+++ connectsynth/utils.py
@@ -4,7 +4,7 @@ class ForceSslMiddleware(object):
     
     def process_request(self, request):
         
-        if not request.is_secure():
+        if request.META['wsgi.url_scheme']=='http':
             return HttpResponseRedirect("https://%s%s" %(
                 request.META['HTTP_HOST'],
                 request.META['PATH_INFO']
