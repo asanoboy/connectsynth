62aa260fbe06c7b76a034937a3f1947df3567416
Yosuke Asano <y.a.personal@gmail.com>
2012-12-04 00:24:22 +0900

    change

--- connectsynth/utils.py
+++ connectsynth/utils.py
@@ -3,12 +3,15 @@ from django.http import HttpResponseRedirect
 class ForceSslMiddleware(object):
     
     def process_request(self, request):
+        print "secure" if request.is_secure() else "not secure"
         print request.META['wsgi.url_scheme']
         
         if request.META['wsgi.url_scheme']!='https':
-            return HttpResponseRedirect("https://%s%s" %(
+            next = "https://%s%s" %(
                 request.META['HTTP_HOST'],
                 request.META['PATH_INFO']
-            ))
+            )
+            print next
+            return HttpResponseRedirect(next)
         
         return None
