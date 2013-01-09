005c7598c3dfc77b2ce3baabb10031e52aeabda1
asano <y.a.personal@gmail.com>
2013-01-09 21:47:16 +0900

    velocity

--- static/develop/synthjs/audiocore/test_wave_worker.js
+++ static/develop/synthjs/audiocore/test_wave_worker.js
@@ -25,12 +25,8 @@ function onGetWave(e){
 var Wave = function(arr, opt_tableSize){
 	
 	this._singingNotes = [];
-	
-	/**
-	 * 鳴っていたNoteが消えた際の後処理を詰め込んだArray Buffer
-	 */
-	this._restBuffer = new Float32Array(0);
-	
+
+	this._removedNotes = [];	
 	this._num = Math.random();
 };
 
@@ -46,15 +42,15 @@ Wave.prototype.addEvent = function(event){
 		this._singingNotes.push({
 			radianPerSample: 440 * Math.pow(2, event.note/12-5) / this._sampleRate * 2 * Math.PI,
 			note: event.note,
-			offsetRadian: 0
+			offsetRadian: 0,
+			velocity: event.velocity
 		});
 	}
 	else if( event.type=='noteoff' || (event.type=='noteon' && event.velocity===0) ){
-		for(var i=0; i<this._singingNotes.length; i++){
-			if( this._singingNotes[i].note == event.note ){
-				this._singingNotes.splice(i, 1);
-			}
-		}
+		this._removedNotes.push({
+			note: event.note,
+			isRemoved: false
+		});
 	}
 	else if( event.type=='notealloff' ){
 		this._singingNotes = [];
@@ -63,24 +59,61 @@ Wave.prototype.addEvent = function(event){
 
 Wave.prototype.getBuffer = function(len){
 	
-	var arr = new Float32Array(len);
+	var arr = new Float32Array(len), theta, lastAmp, amp, isRemoved;
 	
 	for( var i=0; i<this._singingNotes.length; i++ ){
+		isRemoved = false;
+		for( var m=0; m<this._removedNotes.length; m++){
+			//postMessage(this._removedNotes);
+			if( this._singingNotes[i].note==this._removedNotes[m].note ){
+
+				isRemoved = true;
+				break;
+			}
+		}
 		for( var j=0; j<len; j++ ){
-			arr[j] += 0.1 * Math.sin( this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian
-				+ 0.5*Math.sin(3 * (this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian)
-					+ 0.5*Math.sin(7 * (this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian))
-				));
+			theta = this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian;
+			amp = Math.sin(theta);
+			if( isRemoved ){
+				this._singingNotes[i].velocity -= 0.001;
+				if(  this._singingNotes[i].velocity < 0 ){
+					this._removedNotes[m].isRemoved = true;
+					break;
+				}
+			}
+
+			// arr[j] = 0.1 * Math.sin(this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian );
+			arr[j] = 0.1 * this._singingNotes[i].velocity * Math.sin(this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian );
+			// arr[j] += 0.1 * Math.sin( this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian
+			// 	+ 0.5*Math.sin(3 * (this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian)
+			// 		+ 0.5*Math.sin(7 * (this._singingNotes[i].radianPerSample * j + this._singingNotes[i].offsetRadian))
+				// ));
 		}
 		this._singingNotes[i].offsetRadian += this._singingNotes[i].radianPerSample * len;
+
 	}
-	
-	var restLen = len<this._restBuffer.length ? len : this._restBuffer.length;
-	for( i=0; i<restLen; i++ ){
-		arr[i] += this._restBuffer[i];
+
+	var newNotes = [], newRemovedNotes = [];
+	for( i=0; i<this._singingNotes.length; i++ ){
+		isRemoved = false;
+		for( j=0; j<this._removedNotes.length; j++ ){
+			if( this._removedNotes[j].isRemoved && this._removedNotes[j].note==this._singingNotes[i].note ){
+				isRemoved = true;
+				break;
+			}
+		}
+		if( !isRemoved ){
+			newNotes.push(this._singingNotes[i]);
+		}
 	}
+	for( i=0; i<this._removedNotes.length; i++ ){
+		if( !this._removedNotes[i].isRemoved ){
+			newRemovedNotes.push(this._removedNotes[i]);
+		}
+	}
+	this._singingNotes = newNotes;
+	this._removedNotes = newRemovedNotes;
 	
-	this._restBuffer = this._restBuffer.subarray(restLen);
 	
 	return arr;
 };
