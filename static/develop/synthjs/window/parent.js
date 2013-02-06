3a5a0cde72bf846c5844b7b85261757306c1d80e
asano <y.a.personal@gmail.com>
2013-02-07 00:15:20 +0900

    Added window.Child and window.Parent.

---
+++ static/develop/synthjs/window/parent.js
@@ -0,0 +1,18 @@
+goog.provide("synthjs.window.Parent");
+
+
+goog.scope(function(){
+
+var Parent = synthjs.window.Parent = function(){
+
+};
+
+goog.object.extend(Parent.prototype, {
+    postAndWaitDeferred: function(){},
+    post: function(){
+        
+    }
+
+});
+
+});
